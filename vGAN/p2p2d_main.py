8c9d4d953e5fae2471c641e729b0b1af810a62cb
fabiancampos <fabian.camposmontero@deltares.nl>
2023-03-02 11:45:55 +0100

    1st baseline p2p model run

--- vGAN/p2p2d_main.py
+++ vGAN/p2p2d_main.py
@@ -15,34 +15,49 @@ import cv2
 import numpy as np
 from matplotlib import pyplot
 
+from p2p2d_model import read_all_csv_files, apply_miss_rate_per_rf
+
 # Resizing images, if needed
 SIZE_X = 256
 SIZE_Y = 256
+path = 'D:/inpt/synthetic_data/cs2d_256'
 # n_classes=4 #Number of classes for segmentation
 
 # Capture training image info as a list
 tar_images = []
-
-for directory_path in glob.glob("sandstone/images/"):
-    for img_path in glob.glob(os.path.join(directory_path, "*.tif")):
-        img = cv2.imread(img_path, 1)
-        img = cv2.resize(img, (SIZE_Y, SIZE_X))
-        tar_images.append(img)
+# for directory_path in glob.glob(path):
+#     for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+#         img = cv2.imread(img_path, 1)
+#         img = cv2.resize(img, (SIZE_Y, SIZE_X))
+#         tar_images.append(img)
 
 # Convert list to array for machine learning processing
-tar_images = np.array(tar_images)
+# tar_images = np.array(tar_images)
+
 
 # Capture mask/label info as a list
 src_images = []
-for directory_path in glob.glob("sandstone/masks/"):
-    for mask_path in glob.glob(os.path.join(directory_path, "*.tif")):
-        mask = cv2.imread(mask_path, 1)
-        mask = cv2.resize(mask, (SIZE_Y, SIZE_X),
-                          interpolation=cv2.INTER_NEAREST)  # Otherwise ground truth changes due to interpolation
-        src_images.append(mask)
+# for directory_path in glob.glob(path):
+#     for mask_path in glob.glob(os.path.join(directory_path, "*.png")):
+#         mask = cv2.imread(mask_path, 1)
+#         mask = cv2.resize(mask, (SIZE_Y, SIZE_X),
+#                           interpolation=cv2.INTER_NEAREST)  # Otherwise ground truth changes due to interpolation
+#         src_images.append(mask)
 
 # Convert list to array for machine learning processing
-src_images = np.array(src_images)
+# src_images = np.array(src_images)
+
+all_csv = read_all_csv_files(path)
+missing_data, full_data= apply_miss_rate_per_rf(all_csv)
+
+
+
+missing_data = np.array([np.reshape(i, (256, 256)).astype(np.float32) for i in missing_data])
+full_data = np.array([np.reshape(i, (256, 256)).astype(np.float32) for i in full_data])
+
+tar_images = np.reshape(full_data, (1000, 256, 256, 1))
+src_images = np.reshape(missing_data, (1000, 256, 256, 1))
+
 
 print(np.unique(src_images))
 
@@ -50,17 +65,17 @@ n_samples = 3
 for i in range(n_samples):
     pyplot.subplot(2, n_samples, 1 + i)
     pyplot.axis('off')
-    pyplot.imshow(src_images[i])
+    pyplot.imshow(src_images[i], cmap='viridis')
 # plot target image
 for i in range(n_samples):
     pyplot.subplot(2, n_samples, 1 + n_samples + i)
     pyplot.axis('off')
-    pyplot.imshow(tar_images[i])
+    pyplot.imshow(tar_images[i], cmap='viridis')
 pyplot.show()
 
 #######################################################
 
-from pix2pix_model import define_discriminator, define_generator, define_gan, train
+from p2p2d_model import define_discriminator, define_generator, define_gan, train
 
 # define input shape based on the loaded dataset
 image_shape = src_images.shape[1:]
@@ -105,7 +120,7 @@ print("Execution time is: ", execution_time)
 
 # Reports parameters for each batch (total 1096) for each epoch.
 # For 10 epochs we should see 10960
-g_model.save('sandstone_.h5')
+g_model.save('256by256_.h5')
 #########################################
 # Test trained model on a few images...
 
--- vGAN/p2p2d_model.py
+++ vGAN/p2p2d_model.py
@@ -27,13 +27,17 @@ After the last layer, a convolution is applied to map to
 a 1-dimensional output, followed by a Sigmoid function.
 """
 #
+import os
+import random
+import pandas as pd
+import numpy as np
 from numpy import zeros
 from numpy import ones
 from numpy.random import randint
 from keras.optimizers import Adam
 from keras.initializers import RandomNormal
 from keras.models import Model
-from keras.models import Input
+from keras import Input
 from keras.layers import Conv2D
 from keras.layers import Conv2DTranspose
 from keras.layers import LeakyReLU
@@ -61,6 +65,57 @@ from tensorflow.keras.utils import plot_model
 
 # From the paper C64-C128-C256-C512
 # After the last layer, conv to 1-dimensional output, followed by a Sigmoid function.
+
+# read all csv data
+def read_all_csv_files(directory):
+    directory = os.path.join(directory)    # Join the directory path with the os path separator
+    dfs = []    # Create an empty list to store the read dataframes
+
+    # Iterate through all the files in the directory using os.walk()
+    for root, dirs, files in os.walk(directory):
+        for file in files:  # Iterate through all the files and directories in the current root directory
+            if file.endswith(".csv"):   # Check if the file ends with .csv
+                df = pd.read_csv(os.path.join(directory, file), delimiter=',')
+                dfs.append(df)  # Append the dataframe to the list of dataframes
+
+    return dfs      # Return the list of dataframes
+
+
+def apply_miss_rate_per_rf(dfs, miss_rate=0.8):
+    missing_data, full_data = [], []     # Create two empty lists to store missing and full data
+    value_name = 'IC'   # Set value_name to 'IC'
+
+    # Iterate through each random forest in the list
+    for counter, rf in enumerate(dfs):
+        data_z = []     # Create an empty list to store data for each value of x
+        grouped = rf.groupby("z")   # Group the rows of the random field by the value of x
+
+        # Iterate through each group
+        for name, group in grouped:
+            data_z.append(list(group[value_name]))  # Append the 'IC' column of the group to the data_x list
+
+        data_z = np.array(data_z, dtype=float)  # Convert the data_x list to a numpy array of type float
+        no, dim = data_z.shape  # Get the number of rows and columns in the data_x array
+        data_m = remove_random_columns(data_z, miss_rate)   # Call the remove_random_columns function to remove columns from data_x
+        missing_data.append(data_m) # Append the missing data to the missing_data list
+        full_data.append(data_z)    # Append the full data to the full_data list
+
+    # Return the missing_data and full_data lists
+    return missing_data, full_data
+
+
+def remove_random_columns(data_z, miss_rate):
+    data_z = np.transpose(data_z)
+    dim_choice = int(data_z.shape[0])
+    missing_columns_index = random.sample(range(dim_choice), int(miss_rate*dim_choice))
+    data_m = np.ones_like(data_z)
+    for column_index in missing_columns_index:
+        data_m[column_index, :] = np.zeros_like(data_m[column_index, :])
+    miss_list = np.multiply(data_z, data_m)
+    miss_list = np.transpose(miss_list)
+
+    return miss_list
+
 
 def define_discriminator(image_shape):
     # weight initialization
@@ -147,7 +202,7 @@ def decoder_block(layer_in, skip_in, n_filters, dropout=True):
 
 
 # define the standalone generator model - U-net
-def define_generator(image_shape=(256, 256, 3)):
+def define_generator(image_shape=(256, 256, 1)):
     # weight initialization
     init = RandomNormal(stddev=0.02)
     # image input
@@ -293,7 +348,7 @@ def train(d_model, g_model, gan_model, dataset, n_epochs=100, n_batch=1):
         # update the generator
         g_loss, _, _ = gan_model.train_on_batch(X_realA, [y_real, X_realB])
         # summarize performance
-        print('>%d, d1[%.3f] d2[%.3f] g[%.3f]' % (i + 1, d_loss1, d_loss2, g_loss))
+        print('Training>%d, d1[%.3f] d2[%.3f] g[%.3f]' % (i + 1, d_loss1, d_loss2, g_loss))
         # summarize model performance
         if (i + 1) % (bat_per_epo * 10) == 0:
             summarize_performance(i, g_model, dataset)
