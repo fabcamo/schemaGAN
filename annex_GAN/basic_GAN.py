cce937c2eb8830149afcd19951e410394d4037c9
fabiancampos <fabian.camposmontero@deltares.nl>
2023-03-06 11:50:13 +0100

    basic annex GAN on the works

--- annex_GAN/basic_GAN.py
+++ annex_GAN/basic_GAN.py
@@ -1,79 +1,106 @@
-import random
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from matplotlib import pyplot as plt
-
-from tensorflow.keras import Input
-from tensorflow.optimizers import Adam
-from tensorflow.models import Model
-from tensorflow.models import Sequential
-from tensorflow.datasets import mnist
-from tensorflow.utils import to_categorical
-
-from tensorflow.layers import Dense
-from tensorflow.layers import Reshape
-from tensorflow.layers import Flatten
-from tensorflow.layers import Conv2D
-from tensorflow.layers import Conv2DTranspose
-from tensorflow.layers import LeakyReLU
-from tensorflow.layers import Activation
-from tensorflow.layers import Concatenate
-from tensorflowkeras.layers import Dropout
 
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, MaxPooling2D, LeakyReLU, MaxPooling2D
+from tensorflow.keras.layers import InputLayer, Reshape, Flatten, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 
 # Check if GPUs are available
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-
-# Constants and hyerparameters
-
-batch_size = 64
-num_channels = 1
-num_classes = 10
-image_size = 28
-latent_dim = 128
-
-
-# import the data
+# Import the data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-all_digits = np.concatenate([x_train, x_test])
+all_images = np.concatenate([x_train, x_test])
 all_labels = np.concatenate([y_train, y_test])
 
+# Check the image size
+print('The shape of a single image is: ',all_images[0].shape)
 
-
-# Visualize the data
-fig, ax = plt.subplots(1, 10, figsize=(8, 4))
-for i in range(10):
-    rand_idx = random.randint(0, 70000)
-    ax[i].imshow(all_digits[rand_idx], cmap='Greys')
-    ax[i].axis('off')
+# Plot to check the data
+fig, axes = plt.subplots(1, 6, figsize=(10, 3))
+for i in range(6):
+    axes[i].imshow(all_images[i], cmap='Greys')
+    axes[i].axis('off')
 plt.show()
 
-# Check the shape of the images and labels
-print(f"Shape of training images: {all_digits.shape}")
-print(f"Shape of training labels: {all_labels.shape}")
+# Normalize the data between 0 - 1
+all_images = all_images.astype("float32") / 255.0
 
-# create the discriminator
-def discriminator(in_shape=(28,28,1)):
+
+# we need to add an additional dimension to make it know how many channels are there
+all_images = all_images.reshape(70000,28,28,1)
+input_shape = all_images[0].shape
+print(input_shape)
+
+# HYPER-PARAMETERS
+latent_dim = 128        # user defined number as input to the generator
+
+
+
+
+# Discriminator architecture model
+def discriminator_model(input_shape):
     model = Sequential()
-    model.add(InputLayer(28,28,1))
+    # Layer to be used as an entry point into a Network
+    model.add(InputLayer(input_shape=input_shape))
+    # Add convolutional layer
     model.add(Conv2D(filters=128, kernel_size=(3,3), strides=(2,2), padding='same'))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(MaxPool2D(pool_size=(3, 3)), strides=(2, 2))
-    model.add(Flatten(input_shape=(28, 28)))
+    # Add Max Pooling layer
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'))
+    # Flatten the 2D input
+    model.add(Flatten())
+    # Drop 40% of the data
     model.add(Dropout(0.4))
+    # Add the Dense layers
     model.add(Dense(units=128))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Dense(units=64))
-    model.add(LeakyReLU(alpha=0.2))
-    # add the FINAL dense layer for Binary Classification (fake/real)
+    # Add FINAL fake or real layer
     model.add(Dense(units=1, activation='sigmoid'))
+
+    # Define the optimizer
     opt = Adam(lr=0.0002, beta_1=0.5)
-    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-    # return
+    # Compile the model
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['acuracy'])
+
     return model
 
-def generator(latent_dim):
+
+#test_discr = discriminator_model(input_shape)
+#print(test_discr.summary())
+
+# Generator architecture model
+def generator_model(latent_dim):
     model = Sequential()
+
+    # Define initial geometry vector in order to, after all operations
+    # arrive at a dimension that is equal to the input image
+    n_nodes = 7 * 7 * 128
+
+    # Layer to be used as an entry point into a Network
+    model.add(InputLayer(input_shape=latent_dim))
+    # Add Dense layer starting from a given number of neurons
+    model.add(Dense(units=n_nodes))
+    model.add(LeakyReLU(alpha=0.2))
+
+    # Reshape from 1D to 2D to apply Conv2D
+    model.add(Reshape((7, 7, 128)))
+
+    model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))  # 16x16x128
+    model.add(LeakyReLU(alpha=0.2))
+
+    model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same'))  # 32x32x128
+    model.add(LeakyReLU(alpha=0.2))
+    # generate
+    model.add(Conv2D(1, (8, 8), activation='tanh', padding='same'))  # 32x32x3
+    return model  # Model not compiled as it is not directly trained like the discriminator.
+    # Generator is trained via GAN combined model.
+
+    return model
+
+test_gen = generator_model(128)
+print(test_gen.summary())
