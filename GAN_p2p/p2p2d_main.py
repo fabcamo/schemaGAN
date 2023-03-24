8b39ab06a6a3a2472ca1455c1d61df155f721f2f
Fabian.CamposMontero <Fabian.CamposMontero@deltares.nl>
2023-03-24 02:27:32 +0100

    bring the changes in the remove functions to the p2p script

--- GAN_p2p/p2p2d_main.py
+++ GAN_p2p/p2p2d_main.py
@@ -1,6 +1,9 @@
 import cv2
+from datetime import datetime
 import numpy as np
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
+from numpy.random import randint
+from numpy import vstack
 
 from p2p2d_model_256 import read_all_csv_files, apply_miss_rate_per_rf
 from p2p2d_model_256 import define_discriminator, define_generator, define_gan, train
@@ -10,7 +13,10 @@ SIZE_X = 256
 SIZE_Y = 64
 no_rows = SIZE_Y
 no_cols = SIZE_X
-path = '/scratch/cs2d'
+path = 'C:\inpt\synthetic_data\cs2d_test'
+
+miss_rate = 0.90
+min_distance = 6
 
 # n_classes=4 #Number of classes for segmentation
 
@@ -23,26 +29,26 @@ src_images = []
 # src_images = np.array(src_images)
 
 all_csv = read_all_csv_files(path)
-missing_data, full_data= apply_miss_rate_per_rf(all_csv)
+missing_data, full_data= apply_miss_rate_per_rf(all_csv, miss_rate, min_distance)
 
 
 missing_data = np.array([np.reshape(i, (no_rows, no_cols)).astype(np.float32) for i in missing_data])
 full_data = np.array([np.reshape(i, (no_rows, no_cols)).astype(np.float32) for i in full_data])
 
-tar_images = np.reshape(full_data, (1000, no_rows, no_cols, 1))
-src_images = np.reshape(missing_data, (1000, no_rows, no_cols, 1))
+tar_images = np.reshape(full_data, (6, no_rows, no_cols, 1))
+src_images = np.reshape(missing_data, (6, no_rows, no_cols, 1))
 
 n_samples = 3
 for i in range(n_samples):
-    pyplot.subplot(2, n_samples, 1 + i)
-    pyplot.axis('off')
-    pyplot.imshow(src_images[i], cmap='viridis')
+    plt.subplot(2, n_samples, 1 + i)
+    plt.axis('off')
+    plt.imshow(src_images[i], cmap='viridis')
 # plot target image
 for i in range(n_samples):
-    pyplot.subplot(2, n_samples, 1 + n_samples + i)
-    pyplot.axis('off')
-    pyplot.imshow(tar_images[i], cmap='viridis')
-pyplot.show()
+    plt.subplot(2, n_samples, 1 + n_samples + i)
+    plt.axis('off')
+    plt.imshow(tar_images[i], cmap='viridis')
+plt.show()
 
 #######################################################
 
@@ -76,7 +82,7 @@ def preprocess_data(data):
 
 dataset = preprocess_data(data)
 
-from datetime import datetime
+
 
 start1 = datetime.now()
 
@@ -95,9 +101,7 @@ g_model.save('256by64_.h5')
 #########################################
 # Test trained model on a few images...
 
-from keras.models import load_model
-from numpy.random import randint
-from numpy import vstack
+
 
 #model = load_model('sandstone_50epochs.h5')
 
@@ -111,14 +115,14 @@ def plot_images(src_img, gen_img, tar_img):
     # plot images row by row
     for i in range(len(images)):
         # define subplot
-        pyplot.subplot(1, 3, 1 + i)
+        plt.subplot(1, 3, 1 + i)
         # turn off axis
-        pyplot.axis('off')
+        plt.axis('off')
         # plot raw pixel data
-        pyplot.imshow(images[i, :, :, 0], cmap='gray')
+        plt.imshow(images[i, :, :, 0], cmap='gray')
         # show title
-        pyplot.title(titles[i])
-    pyplot.show()
+        plt.title(titles[i])
+    plt.show()
 
 
 [X1, X2] = dataset
@@ -140,4 +144,4 @@ test_src_img = np.expand_dims(test_src_img, axis=0)
 gen_test_image = model.predict(test_src_img)
 
 # pyplot.imshow(test_src_img[0, :,:,0], cmap='gray')
-pyplot.imshow(gen_test_image[0, :, :, 0], cmap='gray')
+plt.imshow(gen_test_image[0, :, :, 0], cmap='gray')
