fixed normalization> test on Delftblue

GAN_p2p/functions/p2p_summary.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from functions.p2p_generate_samples import generate_real_samples, generate_real_samples_fix, generate_fake_samples
-from functions.p2p_process_data import reverse_normalization
+from functions.p2p_process_data import reverse_normalization, reverse_IC_normalization
 
-results_dir_path = r'/scratch/fcamposmontero/p2p_512x32_results10'
+results_dir_path = r'/scratch/fcamposmontero/results_p2p/512x32_e200_s2000'
 #results_dir_path = r'C:\inpt\GAN_p2p\results\test'
 #results_dir_path = r'/scratch/fcamposmontero/p2p_512x32_results_test'
 
@@ -123,7 +123,7 @@ def plot_history(d_hist, g_hist, g_epoch_hist, d_epoch_hist, a1_hist, a2_hist, a
     ax1.set_xlim([0, iterations])
     ax2.set_xlabel('Epochs')
     ax2.set_xlim([0, n_epochs])
-    ax1.set_ylabel('Accuracy')
+    ax1.set_ylabel('MAE')
     ax1.set_ylim([0, 1])
     ax1.legend(loc='upper right')
     ax2.legend(loc='lower right')
@@ -162,7 +162,7 @@ def plot_images(src_img, gen_img, tar_img):
     images = np.vstack((src_img, gen_img, tar_img))
 
     # Scale from [-1,1] to [0,255]
-    images = reverse_normalization(images)
+    images = reverse_IC_normalization(images)
 
     # Set plot titles
     titles = ['Input', 'Output-Generated', 'Original']
@@ -176,7 +176,8 @@ def plot_images(src_img, gen_img, tar_img):
         # define subplot
         ax = fig.add_subplot(1, 3, 1 + i)
         # plot raw pixel data
-        im = ax.imshow(images[i, :, :, 0], cmap='viridis', vmin=ranges_vmin_vmax[i][0], vmax=ranges_vmin_vmax[i][1])
+        #im = ax.imshow(images[i, :, :, 0], cmap='viridis', vmin=ranges_vmin_vmax[i][0], vmax=ranges_vmin_vmax[i][1])
+        im = ax.imshow(images[i, :, :, 0], cmap='viridis')
         # set title with fontsize
         ax.set_title(titles[i], fontsize=10)
         # set tick_params with fontsize
@@ -208,7 +209,7 @@ def plot_images_error(src_img, gen_img, tar_img):
     images = np.vstack((src_img, gen_img, tar_img, np.abs(gen_img-tar_img)))
 
     # Scale from [-1,1] to [0,255]
-    images = reverse_normalization(images)
+    images = reverse_IC_normalization(images)
     # Set plot titles
     titles = ['Input', 'Output-Generated', 'Original', 'MAE']
     ranges_vmin_vmax = [[1.3, 4.2], [1.3, 4.2], [1.3, 4.2], [110, 130]]

GAN_p2p/p2p2d_main.py
@@ -5,13 +5,11 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-from functions.p2p_process_data import read_all_csv_files, apply_miss_rate_per_rf, preprocess_data
+from functions.p2p_process_data import read_all_csv_files, apply_miss_rate_per_rf, preprocess_data, IC_normalization
 from functions.p2p_discriminator_architecture import define_discriminator_512x32, define_discriminator
 from functions.p2p_generator_architecture import define_generator
 from functions.p2p_gan_architecture import define_gan
 from functions.p2p_train_architecture import train
-
-
 
 
 # Resizing images, if needed
@@ -21,8 +19,8 @@ no_rows = SIZE_Y
 no_cols = SIZE_X
 
 # Define the paths
-path = r'/scratch/fcamposmontero/512x32_reduced'
-path_results = r'/scratch/fcamposmontero/p2p_512x32_results10'
+path = r'/scratch/fcamposmontero/databases/512x32_2k'
+path_results = r'/scratch/fcamposmontero/results_p2p/512x32_e200_s2000'
 
 #path = r'/scratch/fcamposmontero/512x32/training'
 #path_results = r'/scratch/fcamposmontero/p2p_512x32_results'
@@ -36,15 +34,22 @@ results_dir_path = os.path.join(path_results, 'results_summary.txt')
 # Check the time and start the timers
 time_current = time.strftime("%d/%m/%Y %H:%M:%S")
 
-
+#################################################################################################################
+#   CHOOSE THE EPOCHS AND MISSING RATE
+#################################################################################################################
 
 #miss_rate = 0.9868
 #min_distance = 51
-miss_rate = 0.90
-min_distance = 6
+miss_rate = 0.95
+min_distance = 10
 
 # Number of epochs
-n_epochs = 500
+n_epochs = 200
+
+
+#################################################################################################################
+#   PROCESS THE DATA AND DEFINE THE MODELS
+#################################################################################################################
 
 # Capture training image info as a list
 tar_images = []
@@ -62,13 +67,8 @@ full_data = np.array([np.reshape(i, (no_rows, no_cols)).astype(np.float32) for i
 tar_images = np.reshape(full_data, (no_samples, no_rows, no_cols, 1))
 src_images = np.reshape(missing_data, (no_samples, no_rows, no_cols, 1))
 
-
-
 # define input shape based on the loaded dataset
 image_shape = src_images.shape[1:]
-
-
-
 
 # define the models
 d_model = define_discriminator_512x32(image_shape)
@@ -81,11 +81,12 @@ gan_model = define_gan(g_model, d_model, image_shape)
 # load and prepare training images
 data = [src_images, tar_images]
 
-
 # Preprocess data to change input range to values between -1 and 1
 # This is because the generator uses tanh activation in the output layer
 # And tanh ranges between -1 and 1
-dataset = preprocess_data(data)
+#dataset = preprocess_data(data)
+dataset = IC_normalization(data)
+
 
 #################################################################################################################
 #   TRAIN THE GAN
@@ -100,9 +101,6 @@ train(d_model, g_model, gan_model, dataset, n_epochs, n_batch=1)
 time_end = time.time() # End the timer
 # Execution time of the model
 execution_time = abs(time_start - time_end) # Calculate the run time
-print("Execution time is: ", execution_time)
-
-
 
 
 #################################################################################################################
@@ -144,25 +142,3 @@ with open(results_dir_path, "a") as file:
     d_model.summary(print_fn=lambda x: file.write(x + '\n'))
     file.write(" \n")
 
-
-
-
-
-
-
-
-'''
-# To print a sample of the input and the original
-n_samples = 3
-for i in range(n_samples):
-    plt.subplot(2, n_samples, 1 + i)
-    plt.axis('off')
-    plt.imshow(src_images[i], cmap='viridis')
-# plot target image
-for i in range(n_samples):
-    plt.subplot(2, n_samples, 1 + n_samples + i)
-    plt.axis('off')
-    plt.imshow(tar_images[i], cmap='viridis')
-plt.show()
-
-'''

GAN_p2p/p2p_load_model.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 import numpy as np
 
-from functions.p2p_process_data import read_all_csv_files, apply_miss_rate_per_rf, preprocess_data
+from functions.p2p_process_data import read_all_csv_files, apply_miss_rate_per_rf, preprocess_data, IC_normalization
 from functions.p2p_summary import plot_images, plot_images_error
 from numpy.random import randn
 
@@ -39,12 +39,12 @@ src_images = np.reshape(missing_data, (no_samples, no_rows, no_cols, 1))
 n_samples = 1
 for i in range(n_samples):
     plt.subplot(2, n_samples, 1 + i)
-    plt.axis('off')
+    #plt.axis('off')
     plt.imshow(src_images[i], cmap='viridis')
 # plot target image
 for i in range(n_samples):
     plt.subplot(2, n_samples, 1 + n_samples + i)
-    plt.axis('off')
+    #plt.axis('off')
     plt.imshow(tar_images[i], cmap='viridis')
 plt.show()
 plt.close()
@@ -53,7 +53,8 @@ plt.close()
 data = [src_images, tar_images]
 
 # Pre-process the data> normalize
-dataset = preprocess_data(data)
+#dataset = preprocess_data(data)    # old one
+dataset = IC_normalization(data)    # new one
 
 # Load the model
 model = load_model('C:\\inpt\\GAN_p2p\\results\\r05_512x32_10\\generators\\model_000099.h5')