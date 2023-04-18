fixing errors with the normalization...

GAN_p2p/functions/p2p_summary.py
@@ -4,8 +4,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-from functions10.p2p_generate_samples import generate_real_samples, generate_real_samples_fix, generate_fake_samples
-from functions10.p2p_process_data import reverse_normalization
+from functions.p2p_generate_samples import generate_real_samples, generate_real_samples_fix, generate_fake_samples
+from functions.p2p_process_data import reverse_normalization
 
 results_dir_path = r'/scratch/fcamposmontero/p2p_512x32_results10'
 #results_dir_path = r'C:\inpt\GAN_p2p\results\test'
@@ -84,7 +84,7 @@ def plot_history(d_hist, g_hist, g_epoch_hist, d_epoch_hist, a1_hist, a2_hist, a
     plt.plot(a2_hist, label='acc-fake', color='darkgray', alpha=0.8)
     plt.legend(loc='upper right')
     plt.xlabel('Iteration')
-    plt.ylabel('Accuracy')
+    plt.ylabel('MAE')
     # set x-axis limits
     plt.xlim([0, iterations])
     # set y-axis limits
@@ -204,19 +204,14 @@ def plot_images(src_img, gen_img, tar_img):
 
 def plot_images_error(src_img, gen_img, tar_img):
 
+
     images = np.vstack((src_img, gen_img, tar_img, np.abs(gen_img-tar_img)))
-    test = np.abs(gen_img-tar_img)
-    print(np.min(test))
-    print(np.max(test))
 
     # Scale from [-1,1] to [0,255]
     images = reverse_normalization(images)
-    print('genimage>', images[1])
-    print('tarimage>', images[2])
-    print('gen-tar>', np.abs(images[1]-images[2]))
     # Set plot titles
-    titles = ['Input', 'Output-Generated', 'Original', 'Error']
-    ranges_vmin_vmax = [[1.3, 4.2], [1.3, 4.2], [1.3, 4.2]]
+    titles = ['Input', 'Output-Generated', 'Original', 'MAE']
+    ranges_vmin_vmax = [[1.3, 4.2], [1.3, 4.2], [1.3, 4.2], [110, 130]]
 
     # Create a figure with a size of 10 inches by 4 inches
     fig = plt.figure(figsize=(10, 15))

synthetic_data/generate_2D_trigo.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from layers_functions.layer_boundary import layer_boundary
 from layers_functions.generate_rf import generate_rf_group
 
-path = 'C:\\inpt\\synthetic_data\\512x32'
+path = 'C:\\inpt\\synthetic_data\\ppt'
 
 seed = 20230412        # seed
 no_realizations = 100     # number of realizations to generate
@@ -15,7 +15,7 @@ no_realizations = 100     # number of realizations to generate
 
 ##### MAIN DIMENSION VARIABLES ######################################################################################
 x_max = 512             # length (x) of the model
-z_max = 32              # depth (z) of the model
+z_max = 64              # depth (z) of the model
 # Model coordinates
 x_coord = np.arange(0, x_max, 1)       # array of x coordinates
 z_coord = np.arange(0, z_max, 1)       # array of z coordinates

synthetic_data/layers_functions/generate_rf.py
@@ -14,6 +14,11 @@ ndim = 2
 # Robertson, P.K., 2009a. Interpretation of cone penetration tests – a unified approach. Canadian Geotechnical Journal, 46:1337-1355.
 # Robertson, P. K., Cabal, K. (2022). Guide to Cone Penetration Testing (7th ed.). Gregg Drilling LLC.
 
+aniso_x = np.random.triangular(3, 47, 80) # anisotropy in X
+aniso_z = np.random.uniform(0.2, 3)  # anisotropy in Z
+
+#aniso_x = np.random.randint(10, 40)  # anisotropy in X
+#aniso_z = aniso_x / np.random.randint(4, 10)  # anisotropy in Z
 
 # RF-IC values for clay
 def soil_behaviour_clay():
@@ -21,10 +26,11 @@ def soil_behaviour_clay():
     max_IC = 3.6
     mean = (min_IC + max_IC) / 2
     std_value = (max_IC - min_IC) / 6
-    aniso_x = np.random.randint(10,40)  # anisotropy in X
-    aniso_z = aniso_x / np.random.randint(4, 10)  # anisotropy in Z
+    aniso_x = np.random.triangular(3, 47, 80)  # anisotropy in X
+    aniso_z = np.random.uniform(0.2, 3)  # anisotropy in Z
     angle_factor = np.random.triangular(20, 80, 100)
     angles = np.pi / angle_factor  # angle of rotation
+    #angles = 0
     return std_value, mean, aniso_x, aniso_z, angles
 
 
@@ -34,10 +40,11 @@ def soil_behaviour_siltmix():
     max_IC = 2.95
     mean = (min_IC + max_IC) / 2
     std_value = (max_IC - min_IC) / 6
-    aniso_x = np.random.randint(10,40)  # anisotropy in X
-    aniso_z = aniso_x / np.random.randint(4, 10)  # anisotropy in Z
+    aniso_x = np.random.triangular(3, 47, 80)  # anisotropy in X
+    aniso_z = np.random.uniform(0.2, 3)  # anisotropy in Z
     angle_factor = np.random.triangular(20, 80, 100)
     angles = np.pi / angle_factor  # angle of rotation
+    #angles = 0
     return std_value, mean, aniso_x, aniso_z, angles
 
 
@@ -47,10 +54,11 @@ def soil_behaviour_sandmix():
     max_IC = 2.6
     mean = (min_IC + max_IC) / 2
     std_value = (max_IC - min_IC) / 6
-    aniso_x = np.random.randint(10,40)  # anisotropy in X
-    aniso_z = aniso_x / np.random.randint(4, 10)  # anisotropy in Z
+    aniso_x = np.random.triangular(3, 47, 80)  # anisotropy in X
+    aniso_z = np.random.uniform(0.2, 3)  # anisotropy in Z
     angle_factor = np.random.triangular(20, 80, 100)
     angles = np.pi / angle_factor  # angle of rotation
+    #angles = 0
     return std_value, mean, aniso_x, aniso_z, angles
 
 
@@ -60,10 +68,11 @@ def soil_behaviour_sand():
     max_IC = 2.05
     mean = (min_IC + max_IC) / 2
     std_value = (max_IC - min_IC) / 6
-    aniso_x = np.random.randint(10,40)  # anisotropy in X
-    aniso_z = aniso_x / np.random.randint(4, 10)  # anisotropy in Z
+    aniso_x = np.random.triangular(3, 47, 80)  # anisotropy in X
+    aniso_z = np.random.uniform(0.2, 3)  # anisotropy in Z
     angle_factor = np.random.triangular(20, 80, 100)
     angles = np.pi / angle_factor  # angle of rotation
+    #angles = 0
     return std_value, mean, aniso_x, aniso_z, angles
 
 
@@ -73,10 +82,11 @@ def soil_behaviour_organic():
     max_IC = 4.2
     mean = (min_IC + max_IC) / 2
     std_value = (max_IC - min_IC) / 6
-    aniso_x = np.random.randint(10,40)  # anisotropy in X
-    aniso_z = aniso_x / np.random.randint(4, 10)  # anisotropy in Z
+    aniso_x = np.random.triangular(3, 47, 80)  # anisotropy in X
+    aniso_z = np.random.uniform(0.2, 3)  # anisotropy in Z
     angle_factor = np.random.triangular(20, 80, 100)
     angles = np.pi / angle_factor  # angle of rotation
+    #angles = 0
     return std_value, mean, aniso_x, aniso_z, angles
 
 

synthetic_data/plot_trig_models_noRF.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from layers_functions.layer_boundary import layer_boundary
 
 x_max = 512             # length (x) of the model
-z_max = 32              # depth (z) of the model
+z_max = 64              # depth (z) of the model
 # Model coordinates
 x_coord = np.arange(0, x_max, 1)       # array of x coordinates
 z_coord = np.arange(0, z_max, 1)       # array of z coordinates
@@ -16,7 +16,7 @@ coords_to_list = np.array([xs.ravel(), zs.ravel()]).T
 values = np.zeros(coords_to_list.shape[0])
 
 # Plot new matrix as image
-fig, axs = plt.subplots(nrows=10, ncols=3, figsize=(20, 40))
+fig, axs = plt.subplots(nrows=10, ncols=3, figsize=(10, 20))
 
 for i in range(50):
     # Generate new y value for each plot
@@ -60,4 +60,6 @@ for i in range(50):
     ax.imshow(new_matrix, cmap='viridis')
 
 plt.tight_layout()
+for ax in axs.flat:
+    ax.axis('off')
 plt.show()